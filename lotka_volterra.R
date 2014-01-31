42a6460f3013fdd8d705013517fa3f9c4e89108d
diogro <diogro@gmail.com>
2014-01-30 22:48:22 -0200

    volteRRa

--- lotka_volterra.R
+++ lotka_volterra.R
@@ -10,7 +10,7 @@ parameters <- c(r = 2, k = 0.5, e = 0.1, d = 1)
 # initial condition: this is an array now!
 state <- c(V = 1, P = 3)
 
-lotkaVoltera <- function(t, state, parameters){
+lotkaVolterra <- function(t, state, parameters){
   with(as.list(c(state, parameters)), {
     dV = r * V - k * V * P
     dP = e * k * V * P - d * P
@@ -18,7 +18,7 @@ lotkaVoltera <- function(t, state, parameters){
   })
 }
 
-out <- ode(y = state, times = t, func = lotkaVoltera, parms = parameters)
+out <- ode(y = state, times = t, func = lotkaVolterra, parms = parameters)
 out_df = as.data.frame(out)
 
 ggplot(data = out_df, aes(x = time, y = V, color= 'Prey')) + geom_point() + geom_point(data = out_df, aes(x = time, y = P, color = 'Predator'))
